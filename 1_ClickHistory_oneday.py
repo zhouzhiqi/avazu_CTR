1c38a38bf9e19b3a0569559b31cf2f4bfabb6173
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-18 23:44:24 +0000

    Adding tarfile member sanitization to extractall()

--- 1_ClickHistory_oneday.py
+++ 1_ClickHistory_oneday.py
@@ -54,7 +54,26 @@ def ExtractData( data_path):
     if 'count.csv' in file_list: return None  #若已经解压, pass
     for file_name in file_list: #把所有.tar.gz解压到data_path
         with tarfile.open(data_path + file_name, 'r:gz') as tar: 
-            tar.extractall(data_path)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, data_path)
 
 
 def GetC17IIndex(data_path, ):
